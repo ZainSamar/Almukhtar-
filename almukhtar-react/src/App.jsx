0b5a884508029f1cbd7dcde0c149073a13cb3113
ZainSamar <zainsamar.dev@gmail.com>
2026-06-22 00:05:25 +0200

    Refactor import path for Landing component

--- almukhtar-react/src/App.jsx
+++ almukhtar-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing.jsx'
+import Landing from './Landing.jsx'
 import Auth from './pages/Auth.jsx'
 
 function App() {

f5089c195a895367de61158a4aa63e055c3b73b0
ZainSamar <zainsamar.dev@gmail.com>
2026-06-22 01:17:49 +0200

    Add Store routes to App component

--- almukhtar-react/src/App.jsx
+++ almukhtar-react/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing.jsx'
 import Auth from './pages/Auth.jsx'
 import SellerDashboard from './pages/SellerDashboard.jsx'
+import Store from './pages/Store.jsx'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/login" element={<Auth />} />
         <Route path="/register" element={<Auth />} />
         <Route path="/dashboard" element={<SellerDashboard />} />
+        <Route path="/store/:id" element={<Store />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </BrowserRouter>
   )
